22e92a1ee626c32f395d3a55ec8fc68b7711242b
Gustavogdea <gustavo.almeida@astagroup.com>
2025-05-27 16:11:37 -0300

    Função Visualizar Refinar

---
+++ Menu/Opcoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AplicandoConhecimentosCursosAlura.Menu;
+
+public class Opcoes
+{
+	public required string Descricao { get; set; }
+	public required Func<object> Acao{ get; set; }
+
+}
--- Model/Cadastro.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AplicandoConhecimentosCursosAlura.Model;
-
-abstract class Cadastro
-{
-    public abstract void AdicionarCadastro();
-
-    protected List<Fornecedor> _listaFornecedores { get; set; } = new List<Fornecedor>();
-    protected List<Cliente> _listaClientes { get; set; } = new List<Cliente>();
-}
